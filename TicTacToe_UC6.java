48f305c6b5cd910b25ba2f26183ef60150c52c1b
nandniyy <ny9494@srmist.edu.in>
2026-04-21 10:21:36 +0530

    Create TicTacToe_UC6.java

---
+++ TicTacToe_UC6.java
@@ -0,0 +1,23 @@
+public class TicTacToe_UC6 {
+
+    static char[][] board = new char[3][3];
+
+    /**
+     * Entry point of the program. Places a sample move
+     * and prints the updated cell value.
+     */
+    public static void main(String[] args) {
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]);
+    }
+
+    /**
+     * Updates the board by placing the given symbol at
+     * the specified row and column.
+     * Input: Row, Column, Symbol
+     * Hint: Assume the move is already validated.
+     */
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+}
